12c481b42a8f2ad3953bb9c8462ae589ec1114ce
Ersin Senzek <ersinsenzek@gmail.com>
2021-01-18 10:42:22 +0300

    entrypoint içerisinden makemigrations, migrate ve collectstatic işlemleri çıkarıldı

--- app/src/entrypoint.sh
+++ app/src/entrypoint.sh
@@ -11,8 +11,8 @@ if [ "$DATABASE" = "postgres" ]; then
 fi
 
 echo "makemigrations, migrate ve collectstatic işlemleri yapılıyor. "
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+# python manage.py makemigrations --noinput
+# python manage.py migrate --noinput
+# python manage.py collectstatic --noinput
 
 exec "$@"
